0faf85938ca0c78afc96baee3d2d285443ead417
mazenr323 <120329119+mazenr323@users.noreply.github.com>
2023-12-11 17:01:16 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app2/project/newsapp.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'project/new_service.dart';
 main() async {
   runApp(MaterialApp(
